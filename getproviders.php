df2c028f2bf5de9c60ae2a33949513ac0aefc044
Federico Marani <federico.marani@ymail.com>
2009-01-29 23:42:47 +0000

    * modify getproviders with PDO

--- getproviders.php
+++ getproviders.php
@@ -3,13 +3,27 @@
 print "getproviders<br/>";
 
 $output = $_REQUEST['output'];
-if (!empty($output)) {
-	if ($output == 'xml') {
-		print "outputting in xml...<br/>";
-	}
-	else if ($output == 'json') {
-		print "outputting in json...<br/>";
-	}
+if (empty($output)) die();
+
+try {
+	$dbh = new PDO('sqlite:file=providers.db');
+	$dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
+} 
+catch (PDOException $e) {
+	print "Database connect error: " . $e->getMessage() . "<br/>";
+	die();
+}
+
+# $action handling...
+$action = $_REQUEST['action'];
+
+bla bla  bla
+
+if ($output == 'xml') {
+	print "outputting in xml...<br/>";
+}
+else if ($output == 'json') {
+	print "outputting in json...<br/>";
 }
 
 print_r ($_REQUEST);
