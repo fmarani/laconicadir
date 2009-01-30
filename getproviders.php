9508ace5f98dbc35543ca67624371b4de4856f88
Federico Marani <federico.marani@ymail.com>
2009-01-30 01:01:55 +0000

    * some actions handling

--- getproviders.php
+++ getproviders.php
@@ -1,23 +1,68 @@
 <?php
 
-print "getproviders<br/>";
+# DB CONNECT HELPER
+function dbconnect() {
+	try {
+		$dbh = new PDO('sqlite:file=providers.db');
+		$dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
+		return $dbh;
+	} 
+	catch (PDOException $e) {
+		print "Database connect error: " . $e->getMessage() . "<br/>";
+		die();
+	}
+}
 
+
+# $output contains the output method (xml,json) 
 $output = $_REQUEST['output'];
 if (empty($output)) die();
 
-try {
-	$dbh = new PDO('sqlite:file=providers.db');
-	$dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
-} 
-catch (PDOException $e) {
-	print "Database connect error: " . $e->getMessage() . "<br/>";
-	die();
-}
 
 # $action handling...
 $action = $_REQUEST['action'];
 
-bla bla  bla
+switch ($action) {
+	case 'all':
+		# GET FULL LIST OF THE PROVIDERS
+		$dbh = dbconnect();
+		$st = $dbh->prepare('SELECT * FROM providers as p JOIN providers_langspecific as pl ON p.id = pl.id_provider');
+		$st->execute();
+		
+		$providers = array();
+		while ($row = $st->fetch()) {
+			# ID,URL,REGURL,LANG,DESC,FULLNAME
+			# STESSOID,STESSOURL,STESSOREGURL,LANG2,DESC2,FULLNAME2
+			if ($providers[$row['id']]) {
+				$providers[$row['id']]['description'][$row['lang']] = $row['description'];
+			}
+			else {
+				$array = array(
+					'id' => $row['id'],
+					'nickname' => $row['nickname'],
+					'description' => array(
+						$row['lang'] => $row['description']
+					)
+				);
+				$providers[$row['id']] = $array;
+			}
+		}
+		# FIXME: handling $_REQUEST['since']
+		print_r($providers);
+		break;
+	case 'search':
+		# SEARCH FOR A SPECIFIC PROVIDER
+		query($_REQUEST['q']);
+		break;
+	case 'getid':
+		# GET A PROVIDER BY ID
+		query($_REQUEST['provider_id']);
+		break;
+	case 'getnick':
+		# GET A PROVIDER BY NICKNAME
+		query($_REQUEST['provider_nickname']);
+		break;
+}
 
 if ($output == 'xml') {
 	print "outputting in xml...<br/>";
@@ -26,6 +71,5 @@ else if ($output == 'json') {
 	print "outputting in json...<br/>";
 }
 
-print_r ($_REQUEST);
 
 ?>
