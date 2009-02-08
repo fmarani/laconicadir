e48c9116a12cadbfb3939596ab96929f0b2d5e35
Federico Marani <federico.marani@ymail.com>
2009-02-08 01:41:16 +0000

    * added opt to register new instances to UI

--- index.php
+++ index.php
@@ -14,12 +14,34 @@ require_once 'support/db.php';
 
 if (isset($_REQUEST['nickname']) and isset($_REQUEST['password'])) {
 	$dbh = dbconnect();
-	$st = $dbh->prepare("SELECT id FROM providers as p JOIN ui_users as u ON p.id = u.id_provider
-			WHERE p.nickname = ? AND u.sha1_password = sha1(?) ");
-	$st->execute(array($_REQUEST['nickname'],$_REQUEST['password']));
-	if ($row = $st->fetch()) {
-		$auth_ok = true;
-		$idpro = $row['id'];
+	if ($_REQUEST['new_instance'] == "new_instance") {
+		// check if the instance already exists
+		$st = $dbh->prepare("SELECT id FROM providers WHERE nickname = ? ");
+		$st->execute(array($_REQUEST['nickname']));
+		if ($st->rowCount() > 0) {
+			$msg = "Another instance with the same nickname is already registered";
+		}
+		else {
+			// insert new instance into the db
+			$st = $dbh->prepare("INSERT INTO providers (nickname) VALUES (?) ");
+			$st->execute(array($_REQUEST['nickname']));
+			$idpro = $dbh->lastInsertId();
+			$st = $dbh->prepare("INSERT INTO ui_users VALUES (?,sha1(?)) ");
+			$st->execute(array($idpro,$_REQUEST['password']));
+			$auth_ok = true;
+		}
+	}
+	else {	
+		// login to an existing instance: check
+		$st = $dbh->prepare("SELECT id FROM providers as p JOIN ui_users as u ON p.id = u.id_provider
+				WHERE p.nickname = ? AND u.sha1_password = sha1(?) ");
+		$st->execute(array($_REQUEST['nickname'],$_REQUEST['password']));
+		if ($row = $st->fetch()) {
+			$auth_ok = true;
+			$idpro = $row['id'];
+		}
+		else
+			$msg = "Authentication failed";
 	}
 	if ($auth_ok) {
 		session_start();
@@ -44,11 +66,9 @@ if (isset($_REQUEST['nickname']) and isset($_REQUEST['password'])) {
 <div class="title">Laconi.ca directory</div><br>
 
 <form method=POST>
-<?php
-if ($_REQUEST['nickname']) {
-	print '<p><div class="errormsg">Authentication failed!</div></p>';
-}
-?>
+
+<p><div class="errormsg"><?=$msg?></div></p>
+
 <ul>
 <li>
 <label for="nickname">Nickname</label>
@@ -57,6 +77,10 @@ if ($_REQUEST['nickname']) {
 <li>
 <label for="password">Password</label>
 <input type="password" id="password" name="password"/>
+</li>
+<li>
+<label for="new_instance">Register new instance</label>
+<input type="checkbox" id="new_instance" name="new_instance" value="new_instance"/>
 </li>
 <li>
 <input type="submit" value="Enter"/>
